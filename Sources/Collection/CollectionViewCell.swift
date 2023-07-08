4a9c8ddc396fe36862c4dc7bc496dd71cee7a23c
Stanislav Berezin <stanislav.berezin@icloud.com>
2023-07-08 16:23:00 +0300

    BerezinSY - CollectionViewCell is created

--- Sources/Collection/CollectionViewCell.swift
+++ Sources/Collection/CollectionViewCell.swift
@@ -1,1 +1,10 @@
+import UIKit
+
+open class CollectionViewCell<T>: UICollectionViewCell {
+    
+    class var id: String { String(describing: Self.self) }
+    class var nib: UINib { UINib(nibName: Self.id, bundle: nil) }
+    
+    open var item: T?
+}
 
