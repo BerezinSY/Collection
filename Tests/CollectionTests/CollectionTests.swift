d492265d1c7ae39f953bd34aa029f7042143299c
Stanislav Berezin <stanislav.berezin@icloud.com>
2023-07-08 15:05:55 +0300

    Initial Commit

---
+++ Tests/CollectionTests/CollectionTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Collection
+
+final class CollectionTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Collection().text, "Hello, World!")
+    }
+}
