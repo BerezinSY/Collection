972f899d04d2e89c936a1a200b5dac731da11888
Stanislav Berezin <stanislav.berezin@icloud.com>
2023-07-08 23:25:19 +0300

    BerezinSY - Layout Model is added

---
+++ Sources/Collection/Models/LayoutModel.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+public protocol LayoutModel {
+    func sizeForItem(at indexPath: IndexPath) -> CGSize
+    func insetForSection(at section: Int) -> UIEdgeInsets
+    func referenceSizeForHeader(in section: Int) -> CGSize
+    func referenceSizeForFooter(in section: Int) -> CGSize
+    func minimumInteritemSpacing(for section: Int) -> CGFloat
+    func minimumLineSpacingForSection(for section: Int) -> CGFloat
+    
+}
+
+extension LayoutModel {
+    func sizeForItem(at indexPath: IndexPath) -> CGSize {
+        CGSize(width: 50, height: 50)
+    }
+    
+    func insetForSection(at section: Int) -> UIEdgeInsets {
+        .zero
+    }
+    
+    func referenceSizeForHeader(in section: Int) -> CGSize {
+        .zero
+    }
+    
+    func referenceSizeForFooter(in section: Int) -> CGSize {
+        .zero
+    }
+    
+    func minimumInteritemSpacing(for section: Int) -> CGFloat {
+        10
+    }
+    
+    func minimumLineSpacingForSection(for section: Int) -> CGFloat {
+        10
+    }
+}
