331242a79c47ef9649ffb302c84aa0d55e5808a5
Stanislav Berezin <stanislav.berezin@icloud.com>
2023-07-08 16:21:46 +0300

    BerezinSY-collection model has created

--- Sources/Collection/CollectionModel.swift
+++ Sources/Collection/CollectionModel.swift
@@ -1,1 +1,8 @@
-
+public protocol CollectionModel {
+    associatedtype Item
+    associatedtype Header
+    
+    var items: [Item] { get }
+    var header: Header { get }
+    var isShowHeader: Bool { get }
+}
