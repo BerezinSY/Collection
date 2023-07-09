920fb96dc7d1b8bcd7950e914b689affb27bed52
Stanislav Berezin <stanislav.berezin@icloud.com>
2023-07-09 15:39:07 +0300

    BerezinSY - техкоммит

---
+++ Sources/Collection/Views/HorizontalScrollCollectionViewCell.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+open class HorizontalScrollCollectionViewCell<T>: CollectionViewCell<T> {
+    open override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        setNeedsLayout()
+        layoutIfNeeded()
+        let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
+        var frame = layoutAttributes.frame
+        frame.size.width = ceil(size.width)
+        layoutAttributes.frame = frame
+        return layoutAttributes
+    }
+}
---
+++ Sources/Collection/Views/VerticalScrollCollectionViewCell.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+open class VerticalScrollCollectionViewCell<T>: CollectionViewCell<T> {
+    open override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        setNeedsLayout()
+        layoutIfNeeded()
+        let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
+        var frame = layoutAttributes.frame
+        frame.size.height = ceil(size.height)
+        layoutAttributes.frame = frame
+        return layoutAttributes
+    }
+}
